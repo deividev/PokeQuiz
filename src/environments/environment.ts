28f5a6ce09ead17d57a7bbec0a6dc94c722eec2b
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-08-10 01:03:51 +0200

    Add url strapi-api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-  
+  production: false,
+  regiterStrapi: 'http://localhost:1337/auth/local/register',
+  loginStrapi: 'http://localhost:1337/auth/local'
 };
 
 /*
