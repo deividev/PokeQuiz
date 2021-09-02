2f4edfaaee3ef3325c0db88383b2aa88ebfd777b
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-09-02 10:45:34 +0200

    Clean code no use

--- src/app/jwt-interceptor.interceptor.ts
+++ src/app/jwt-interceptor.interceptor.ts
@@ -6,19 +6,18 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 import { AuthServiceService } from './auth/services/auth-service.service';
 
 @Injectable()
 export class JwtInterceptorInterceptor implements HttpInterceptor {
 
-  constructor(private injector: Injector, private router: Router, private authService: AuthServiceService) {}
+  constructor(private injector: Injector, private authService: AuthServiceService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     let authService = this.injector.get(this.authService);
     let req = request;
-
+    debugger
     if (authService.getToken()) {
       req = request.clone(  {
         setHeaders: {
