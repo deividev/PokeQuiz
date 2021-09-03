5666f9182807ccf033b3bac8762c60841d16e480
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-09-03 12:34:07 +0200

    Fix user login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthResponse, LoginRequest } from '../../models/user';
 import { AuthServiceService } from '../../services/auth-service.service';
 
@@ -15,20 +17,37 @@ export class LoginComponent implements OnInit {
     "password": "test123456789"
   };;
 
-  constructor(private authService: AuthServiceService) { }
+  formLogin: FormGroup;
+
+  constructor(private authService: AuthServiceService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+
+  createForm(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(9)]),
+    });
   }
 
 
   loginUser(): void {
+    this.loginUserData.identifier = this.formLogin.controls['email'].value;
+    this.loginUserData.password = this.formLogin.controls['password'].value;
     this.authService.loginUser(this.loginUserData)
       .subscribe(
         (res: AuthResponse) => {
           console.log(res);
-          localStorage.setItem('token', res.jwt)
+          localStorage.setItem('token', res.jwt);
+          this.router.navigate(['/home']);
         },
         error => console.log(error)
       )
   }
+
+
 }
