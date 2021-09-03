15d047d163a25b96dafc6a7188650d5b5852dbc3
David Seco Diaz <dsecodi@corp.vodafone.es>
2021-09-03 12:34:44 +0200

    Fix user register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthResponse, RegisterRequest } from '../../models/user';
 import { AuthServiceService } from '../../services/auth-service.service';
 
@@ -15,13 +16,28 @@ export class RegisterComponent implements OnInit {
     "email": "papa@paco.io",
     "password": "Test1895"
   };
+  formRegister: FormGroup;
 
   constructor(private authService: AuthServiceService) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+
+  createForm(): void {
+    this.formRegister = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(9)]),
+    });
+  }
+
+
   registerUser(): void {
+    this.resgisterUserData.username = this.formRegister.controls['username'].value;
+    this.resgisterUserData.email = this.formRegister.controls['email'].value;
+    this.resgisterUserData.password = this.formRegister.controls['password'].value;
     this.authService.registerUser(this.resgisterUserData)
       .subscribe(
         (res: AuthResponse) => {
